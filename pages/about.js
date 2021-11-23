161c79cc08f8d45f93d27ae1746e354dad7d7c6d
Tom <tomtomjonckheere@gmail.com>
2021-11-23 17:44:11 +0100

    added about page

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,32 @@
+import Layout from 'components/layout'
+import Head from 'next/head'
 import Container from '../components/container'
+import PageTitle from '../components/page-title'
+import { getAboutPageData } from '../lib/graphcms'
+
+export default function About({ title, content }) {
+    return (
+        <>
+            <Layout>
+                <Head>
+                    <title>About us</title>
+                </Head>
+                <Container>
+                    <PageTitle pageTitle={title} />
+                    <div dangerouslySetInnerHTML={{__html: content}} />
+                </Container>
+            </Layout>
+        </>
+    )
+}
+
+export async function getStaticProps() {
+    const aboutPageData = (await getAboutPageData())
+    
+    const title = aboutPageData.title
+    const content = aboutPageData.content.html
+
+    return {
+        props: { title, content }
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Container from '../components/container'
 import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
+import Intro from '../components/page-title'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/graphcms'
 import Head from 'next/head'
